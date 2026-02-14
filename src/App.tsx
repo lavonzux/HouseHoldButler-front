cb4097718c24689960a99e1bf3ab6777d9d197c9
Chu-Yi-Ting <luke52070000@gmail.com>
2026-02-14 21:12:56 +0800

    新增登入頁面
    1.新增登入頁面
    2.在首頁以及頁籤加上網站 Logo
    3.public 資料夾新增網站 Logo 靜態文件
    4.建立點擊"立即體驗"導頁至登入頁面

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AuthLayout from './components/Layout/AuthLayout';
 import Landing from './pages/Landing';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Inventory from './pages/Inventory';
 import ItemDetail from './pages/ItemDetail';
@@ -16,6 +17,7 @@ const App: React.FC = () => {
       <Routes>
         {/* 公開首頁 */}
         <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
 
         {/* 需要登入的頁面（目前模擬全部開放） */}
         <Route element={<AuthLayout />}>
@@ -26,7 +28,7 @@ const App: React.FC = () => {
           <Route path="/budget" element={<Budget />} />
           <Route path="/settings" element={<Settings />} />
 
-          {/* 預設跳轉到 dashboard */}
+          {/* 預設 404 跳轉到 dashboard */}
           <Route path="*" element={<Navigate to="/dashboard" replace />} />
         </Route>
       </Routes>
